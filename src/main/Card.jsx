77403d69010ce0bd3561288e91bf15bb25268834
Sung Yi <sy@Sungs-Air.fios-router.home>
2023-08-20 13:15:36 -0400

    still working

--- src/main/Card.jsx
+++ src/main/Card.jsx
@@ -1,3 +1,4 @@
+import images from "../images/index.js";
 
 /*"id": "63ebd220e80158f7948b8cb4",
     "title": "A Sunny Day in Seattle",
@@ -7,11 +8,12 @@
     "updatedAt": "2022-04-12T02:58:15.102Z"*/
 export default function Card({elem}){
     return(
-        <div className="card" key={elem.id} >
-            {/*<img src="..." className="card-img-top" alt="..." />*/}
+        <div className="card w-50" key={elem.id} >
+            {console.log(images[`${elem.location.toLowerCase()}`])}{console.log(images)}{console.log(elem.location)}
+            <img src={`${images[elem.location.toLowerCase()]}`} className="card-img-top" />
             <div className="card-body">
                 <h5 className="card-title">{elem.title}</h5>
-                <p className="card-text">{elem.content}</p>
+                <p className="card-text text-truncate">{elem.content}</p>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
